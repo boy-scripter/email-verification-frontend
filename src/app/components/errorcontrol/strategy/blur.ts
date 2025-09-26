3a328d2fda6819b9cd83606f5287640aed67326b
shivam <trickloot8.1@gmail.com>
2025-09-26 13:11:37 +0530

    optimzed stop emitting after true

--- src/app/components/errorcontrol/strategy/blur.ts
+++ src/app/components/errorcontrol/strategy/blur.ts
@@ -3,13 +3,14 @@
 import { ValidationVisibilityStrategy } from './abstract';
 import { AbstractControl } from '@angular/forms';
 import { Observable } from 'rxjs';
-import { map, startWith } from 'rxjs/operators';
+import { map, startWith, takeWhile } from 'rxjs/operators';
 
 export class BlurStrategy implements ValidationVisibilityStrategy {
   showError(control: AbstractControl): Observable<boolean> {
     return control.statusChanges.pipe(
       map(() => control.touched && control.invalid),
-      startWith(control.touched && control.invalid)
+      startWith(control.touched && control.invalid),
+      takeWhile(value => !value, true)
     );
   }
 }
--- src/app/components/errorcontrol/strategy/change.ts
+++ src/app/components/errorcontrol/strategy/change.ts
@@ -1,13 +1,15 @@
 import { ValidationVisibilityStrategy } from './abstract';
 import { AbstractControl } from '@angular/forms';
 import { Observable } from 'rxjs';
-import { map, startWith } from 'rxjs/operators';
+import { tap, map, startWith, takeWhile } from 'rxjs/operators';
 
 export class ChangeStrategy implements ValidationVisibilityStrategy {
   showError(control: AbstractControl): Observable<boolean> {
     return control.statusChanges.pipe(
+      tap(() => console.log(control.touched , control.invalid)),
       map(() => control.touched && control.invalid),
-      startWith(control.touched && control.invalid)
+      startWith(control.touched && control.invalid),
+      takeWhile(value => !value, true)
     );
   }
 }
